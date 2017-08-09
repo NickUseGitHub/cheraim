ed1323694f1fda7e0d1fa308a9c8a6f614f0e17d
nick <nickt.tnick@gmail.com>
2017-08-10 00:38:12 +0700

    controll all searchBox

--- src/search/SearchBox.js
+++ src/search/SearchBox.js
@@ -4,16 +4,20 @@ import './SearchBox.css';
 export default (props) => {
   const {
     keyword,
-    mapList = [1, 2, 3, 4]
+    mapList,
+    isActive,
+    onClick,
+    onChange,
+    onSubmit
   } = props;
 
   return (
       <div className="SearchBox">
         <div className="SearchBox-Container">
-          <form>
-            <input value={keyword} className="SearchBox-Input"/>
+          <form onSubmit={onSubmit}>
+            <input value={keyword} onClick={onClick} onChange={onChange} className="SearchBox-Input"/>
           </form>
-          <ul className={`SearchBox-Suggestion`}>
+          <ul className={`SearchBox-Suggestion ${isActive ? 'active' : ''}`}>
             {mapList.map(v => <li key={v}>{v}</li>)}
           </ul>
         </div>
--- src/search/SearchBoxContainer.js
+++ src/search/SearchBoxContainer.js
@@ -1,6 +1,25 @@
-import { compose, withState } from 'recompose';
+import { compose, withState, withHandlers } from 'recompose';
+import withClickOutside from 'react-onclickoutside';
 import SearchBox from './SearchBox';
 
 export default compose(
-  withState('keyword', 'updateKeyword', 'Hello')
+  withState('keyword', 'updateKeyword', ''),
+  withState('isActive', 'updateIsActive', false),
+  withState('mapList', 'updateMapList', [1, 2, 3, 4]),
+  withHandlers({
+    onChange: ({ updateKeyword }) => event => {
+      updateKeyword(event.target.value);
+    },
+    onSubmit: ({ keyword }) => event => {
+      event.preventDefault()
+      console.log('keyword', keyword)
+    },
+    onClick: ({ updateIsActive }) => event => {
+      updateIsActive(true);
+    },
+    handleClickOutside: ({ updateIsActive }) => event => {
+      updateIsActive(false);
+    }
+  }),
+  withClickOutside
 )(SearchBox);
