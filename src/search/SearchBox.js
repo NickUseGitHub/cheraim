b626f733468f576b9a0ba3a5a19ed04a5ced6d15
nick <nickt.tnick@gmail.com>
2017-08-09 23:22:48 +0700

    create form in SearchComponent

--- src/search/SearchBox.js
+++ src/search/SearchBox.js
@@ -1,18 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './SearchBox.css';
 
 const mapList = [1, 2, 3, 4];
 const isTrue = false;
 
-export default function (props) {
-  return (
-    <div className="SearchBox">
-      <div className="SearchBox-Container">
-        <input className="SearchBox-Input"/>
-        <ul className={`SearchBox-Suggestion ${isTrue ? 'active' : '' }`}>
-          {mapList.map(v => <li key={v}>{v}</li>)}
-        </ul>
+export default class extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      value: 'insert some map...'
+    };
+    this.onHandleSubmit = this.onHandleSubmit.bind(this);
+  }
+
+  onHandleSubmit (e) {
+    e.preventDefault();
+    alert(this.state.value);
+  }
+
+  render () {
+    return (
+      <div className="SearchBox">
+        <div className="SearchBox-Container">
+          <form onSubmit={this.onHandleSubmit}>
+            <input value={this.state.value} className="SearchBox-Input"/>
+          </form>
+          <ul className={`SearchBox-Suggestion ${isTrue ? 'active' : '' }`}>
+            {mapList.map(v => <li key={v}>{v}</li>)}
+          </ul>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
