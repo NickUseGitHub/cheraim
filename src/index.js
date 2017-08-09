40145c0b76df59f519cce360da2890d8670bf4d3
nick <nickt.tnick@gmail.com>
2017-08-09 22:14:16 +0700

    install reset-css

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
+import 'reset-css/reset.css';
 import './index.css';
 import Main from './Main';
 import registerServiceWorker from './registerServiceWorker';
