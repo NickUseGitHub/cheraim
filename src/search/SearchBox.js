bcb951a3a66397ad01cb89e44d98954d07803807
nick <nickt.tnick@gmail.com>
2017-08-09 23:15:11 +0700

    create searchBox component

--- src/search/SearchBox.js
+++ src/search/SearchBox.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+import './SearchBox.css';
+
+const mapList = [1, 2, 3, 4];
+const isTrue = false;
 
 export default function (props) {
   return (
-    <div>
-      Search
+    <div className="SearchBox">
+      <div className="SearchBox-Container">
+        <input className="SearchBox-Input"/>
+        <ul className={`SearchBox-Suggestion ${isTrue ? 'active' : '' }`}>
+          {mapList.map(v => <li key={v}>{v}</li>)}
+        </ul>
+      </div>
     </div>
-  )
+  );
 }
