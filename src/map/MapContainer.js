ea1c160537b3635d84ba88081295ae8513d01dbd
nick <nickt.tnick@gmail.com>
2017-08-13 17:33:18 +0700

    [google-maps] refactor code

--- src/map/MapContainer.js
+++ src/map/MapContainer.js
@@ -1,4 +1,52 @@
-import { compose } from 'recompose';
+import { compose, withHandlers, withState } from 'recompose';
 import Map from './Map';
 
-export default compose()(Map);
+export default compose(
+  withState('markers', 'updateMarkers', () => [{
+    position: {
+      lat: 13.7281262,
+      lng: 100.5328248
+    },
+    key: `Taiwan`,
+    defaultAnimation: 2,
+  }]),
+  withHandlers({
+    handleMapLoad: () => map => {
+      if (map) {
+        console.log(map.getZoom());
+      }
+    },
+    handleMapClick: () => event => {
+      /**
+       * create marker
+       */
+      // const nextMarkers = [
+      //   ...this.state.markers,
+      //   {
+      //     position: event.latLng,
+      //     defaultAnimation: 2,
+      //     key: Date.now(), // Add a key property for: http://fb.me/react-warning-keys
+      //   },
+      // ];
+      // this.setState({
+      //   markers: nextMarkers,
+      // });
+
+      // if (nextMarkers.length === 3) {
+      //   this.props.toast(
+      //     `Right click on the marker to remove it`,
+      //     `Also check the code!`
+      //   );
+      // }
+    },
+    handleMarkerRightClick: ({ markers, updateMarkers }) => targetMarker => {
+      /**=
+        * All you modify is data, and the view is driven by data.
+        * This is so called data-driven-development. (And yes, it's now in
+        * web front end and even with google maps API.)
+        */
+        const nextMarkers = markers.filter(marker => marker !== targetMarker);
+        updateMarkers(nextMarkers);
+    }
+  })
+)(Map);
--- src/map/map.js
+++ src/map/map.js
@@ -1,73 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import MapGoogleContainer from './MapGoogleContainer';
 
-export default class Map extends Component {
+export default function (props) {
+  const {
+    markers,
+    handleMapLoad,
+    handleMapClick,
+    handleMarkerRightClick
+  } = props
 
-  state = {
-    markers: [{
-      position: {
-        lat: 13.7281262,
-        lng: 100.5328248
-      },
-      key: `Taiwan`,
-      defaultAnimation: 2,
-    }],
-  };
-
-  handleMapLoad = this.handleMapLoad.bind(this);
-  handleMapClick = this.handleMapClick.bind(this);
-  handleMarkerRightClick = this.handleMarkerRightClick.bind(this);
-
-  handleMapLoad(map) {
-    this._mapComponent = map;
-    if (map) {
-      console.log(map.getZoom());
-    }
-  }
-
-  /*
-   * This is called when you click on the map.
-   * Go and try click now.
-   */
-  handleMapClick(event) {
-    console.log('map click!');
-    /**
-     * create marker
-     */
-    // const nextMarkers = [
-    //   ...this.state.markers,
-    //   {
-    //     position: event.latLng,
-    //     defaultAnimation: 2,
-    //     key: Date.now(), // Add a key property for: http://fb.me/react-warning-keys
-    //   },
-    // ];
-    // this.setState({
-    //   markers: nextMarkers,
-    // });
-
-    // if (nextMarkers.length === 3) {
-    //   this.props.toast(
-    //     `Right click on the marker to remove it`,
-    //     `Also check the code!`
-    //   );
-    // }
-  }
-
-  handleMarkerRightClick(targetMarker) {
-    /*
-     * All you modify is data, and the view is driven by data.
-     * This is so called data-driven-development. (And yes, it's now in
-     * web front end and even with google maps API.)
-     */
-    const nextMarkers = this.state.markers.filter(marker => marker !== targetMarker);
-    this.setState({
-      markers: nextMarkers,
-    });
-  }
-
-  render() {
-    return (
+  return (
       <div style={{height: `70vh`}}>
         <MapGoogleContainer
           containerElement={
@@ -76,12 +18,11 @@ export default class Map extends Component {
           mapElement={
             <div style={{ height: `100%` }} />
           }
-          onMapLoad={this.handleMapLoad}
-          onMapClick={this.handleMapClick}
-          markers={this.state.markers}
-          onMarkerRightClick={this.handleMarkerRightClick}
+          onMapLoad={handleMapLoad}
+          onMapClick={handleMapClick}
+          markers={markers}
+          onMarkerRightClick={handleMarkerRightClick}
         />
       </div>
     );
-  }
 }
