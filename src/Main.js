9d1dac40471b232a6a90a83ee95580cbf0c419b0
nick <nickt.tnick@gmail.com>
2017-09-03 15:06:12 +0700

    [redux] create Counter Component

--- src/Main.js
+++ src/Main.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './Main.css';
 import MainMapContainer from './components/MainMapContainer';
+import Counter from './components/counter/Counter'
 
 class Main extends Component {
   render() {
@@ -9,10 +10,7 @@ class Main extends Component {
           <div className="Main-header">
             <img className="Main-logo" src="http://cheraim.com/wp-content/uploads/2015/10/GOGO.png" />
           </div>
-          <div>
-            number: 0
-            <button>Increase Counter</button>
-          </div>
+          <Counter />
           <MainMapContainer />
       </div>
     );
---
+++ src/components/counter/Counter.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default (props) => (
+  <div>
+    number: 0
+    <button>Increase Counter</button>
+  </div>
+);
