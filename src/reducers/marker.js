90026013dd320a5e05f5360345fa4fe9bd08d390
Nick Coder <nickt.tnick@gmail.com>
2017-09-12 21:42:33 +0700

    [db-firebase] create reducer Markers

---
+++ src/reducers/marker.js
@@ -0,0 +1,17 @@
+function addMarker () {}
+function editMarker () {}
+function deleteMarker () {}
+function mapMarkers () {}
+
+export const actionTypes = {
+  ADD_MARKER: addMarker,
+  EDIT_MARKER: editMarker,
+  DELETE_MARKER: deleteMarker,
+  GET_MARKERS: mapMarkers
+};
+
+export default (markers = {}, action) => {
+  const { type } = action;
+
+  return actionTypes[type] !== undefined ? actionTypes[type](action) : {}
+}
