5acc841365ccf93f2b83c02844fd7ed45f59efc4
Nick Coder <nickt.tnick@gmail.com>
2017-09-12 22:59:04 +0700

    [db-firebase] fix bug flag not show

--- src/components/map/MapGoogleContainer.js
+++ src/components/map/MapGoogleContainer.js
@@ -6,7 +6,16 @@ import MapGoogle from './MapGoogle';
 function convertDataFromFirebaseListToMarkersArr (markers) {
   if (!markers) return [];
 
-  return Object.keys(markers).map(key => markers[key]);
+  return Object.keys(markers).map(key => {
+    const iconObj = {
+      1: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png'
+    }
+
+    return {
+      ...markers[key],
+      icon: iconObj[markers[key].icon] || ''
+    }
+  });
 }
 
 export default compose (
