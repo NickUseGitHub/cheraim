4dd37f53b88c6d21644ffccd855695cd5bbb77ba
Nick Coder <nickt.tnick@gmail.com>
2017-09-27 21:44:34 +0700

    [css-window-info] ปรับ css ให้สวยงาม

--- src/components/map/mapModal/MapModal.js
+++ src/components/map/mapModal/MapModal.js
@@ -14,22 +14,23 @@ export default function MapModal (props) {
     onValueChange
   } = props;
 
-  console.log('selectedIcon', selectedIcon)
-
   return (<Modal
+    className={{
+      base: 'map-modal'
+    }}
     isOpen={showModal}
     style={{ overlay: { zIndex: 102 }}}
     contentLabel="Example Modal"
   >
-    <div>Name: <input type="text" value={name} onChange={onValueChange('Name')} /></div>
-    <div>Desciption: <input type="text" value={description} onChange={onValueChange('Description')} /></div>
-    <div>
-      <div>Select Icon</div>
-      <button style={{ backgroundColor: (selectedIcon === 0 ? 'green' : 'white')}} onClick={onIconClick(0)}><img src="http://maps.google.com/mapfiles/marker.png" /></button>
-      <button style={{ backgroundColor: (selectedIcon === 1 ? 'green' : 'white')}} onClick={onIconClick(1)}><img src="https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png" /></button>
-      <button style={{ backgroundColor: (selectedIcon === 2 ? 'green' : 'white')}} onClick={onIconClick(2)}><img src="http://maps.google.com/mapfiles/marker_orange.png" /></button>
+    <div><span className="title">Name:</span><input type="text" value={name} onChange={onValueChange('Name')} /></div>
+    <div><span className="title">Desciption:</span><input type="text" value={description} onChange={onValueChange('Description')} /></div>
+    <div className="select-icon-marker">
+      <div><span className="title">Select Icon</span></div>
+      <button className={ selectedIcon === 0 ? 'active' : '' } onClick={onIconClick(0)}><img src="http://maps.google.com/mapfiles/marker.png" /></button>
+      <button className={ selectedIcon === 1 ? 'active' : '' } onClick={onIconClick(1)}><img src="https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png" /></button>
+      <button className={ selectedIcon === 2 ? 'active' : '' } onClick={onIconClick(2)}><img src="http://maps.google.com/mapfiles/marker_orange.png" /></button>
     </div>
-    <button onClick={onClickSave}>Save</button>
-    <button onClick={() => setShowModal(false)}>Cancel</button>
+    <button className="btn btn-success" onClick={onClickSave}>Save</button>
+    <button className="btn" onClick={() => setShowModal(false)}>Cancel</button>
   </Modal>)
 }
